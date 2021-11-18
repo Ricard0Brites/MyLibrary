6c1bc2a48b769be68014e277b4d370dc1d623095
Ricard0Brites <93977728+Ricard0Brites@users.noreply.github.com>
2021-11-18 22:06:36 +0000

    Create RBL.js

---
+++ RBL.js
@@ -0,0 +1,10 @@
+//Interrupts code for X miliseconds
+function sleep(milliseconds) 
+{
+    const date = Date.now();
+    let currentDate = null;
+    do {
+            currentDate = Date.now();
+       } 
+    while (currentDate - date < milliseconds);
+  }
