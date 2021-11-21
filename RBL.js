02d82967f1510cb177b62b4789398b3d3ec4e40c
Ricard0Brites <93977728+Ricard0Brites@users.noreply.github.com>
2021-11-21 17:25:16 +0000

    Added a function

--- RBL.js
+++ RBL.js
@@ -8,3 +8,20 @@ function sleep(milliseconds)
        } 
     while (currentDate - date < milliseconds);
   }
+
+//get transition end browser based
+function GetTransitionEnd() {
+    var transitions = {
+        //the different browsers' events
+        transition: "transitionend",
+        OTransition: "oTransitionEnd",
+        MozTransition: "trnasitionend",
+        WebkitTransform: "webkitTransitionEnd",
+    };
+    let bodyStyle = document.body.style;
+    for (let transition in transitions) {
+        if (bodyStyle[transition] != undefined) {
+            return transitions[transition];
+        }
+    }
+}
