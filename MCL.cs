a4e9312c3b447abba058f3ded2476aeb28f7d45a
Ricard0Brites <93977728+Ricard0Brites@users.noreply.github.com>
2021-12-24 03:27:50 +0000

    Update
    -updated some variable names to become clearer to the end-user
    -added a comment to explain each variable

--- MCL.cs
+++ MCL.cs
@@ -1,6 +1,10 @@
-    private bool Approx(float valA, float reference, float range)
+// valA ---------> is the value we are given to compare
+// baseLine---> this is the value we want to  achieve
+// range --------> how far off the ValA can be compared to baseLine
+
+private bool Approx(float valA, float baseLine, float range)
     {
-        if(valA + range >= -(reference + range) && valA + range <= reference + range)
+        if(valA + range >= -(baseLine + range) && valA + range <= baseLine + range)
         {
             return true;
         }
